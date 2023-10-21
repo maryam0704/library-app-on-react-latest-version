41791713c9d9d8e97983d116bc833e7506a76fd5
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-10-21 17:46:50 -0400

    delete book without backend

---
+++ src/components/DeleteBook.jsx
@@ -0,0 +1,11 @@
+
+const DeleteBook = ({onDeleteClick }) => {
+  return (
+    <div>
+    
+        <button onClick={onDeleteClick}>Delete</button>
+    </div>
+  );
+};
+
+export default DeleteBook;
